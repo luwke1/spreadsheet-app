57020055786ccedca9211f867c9a98690f4a0054
lflock <lflock03@gmail.com>
2024-09-03 16:39:16 -0700

    Part 1

---
+++ HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter numbers: \n");
+        string input = Console.ReadLine();
+        string[] nums = input.Split(" ");
+
+        // Test if properly processed
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+}
