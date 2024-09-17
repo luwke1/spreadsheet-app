e603e70ff0551989d657b2c914135365f085490a
lflock <lflock03@gmail.com>
2024-09-17 14:20:57 -0700

    Setting up windows form application, with file menu strip

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,17 +29,75 @@
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
+            openFileDialog1 = new OpenFileDialog();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openFileToolStripMenuItem = new ToolStripMenuItem();
+            loadFibonacciNumbersfirst50ToolStripMenuItem = new ToolStripMenuItem();
+            loadFibonacciNumbersfirst100ToolStripMenuItem = new ToolStripMenuItem();
+            saveToFileToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Dock = DockStyle.Fill;
-            textBox1.Location = new Point(0, 0);
+            textBox1.Dock = DockStyle.Bottom;
+            textBox1.Location = new Point(0, 28);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(800, 450);
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(800, 422);
             textBox1.TabIndex = 0;
-            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openFileToolStripMenuItem, loadFibonacciNumbersfirst50ToolStripMenuItem, loadFibonacciNumbersfirst100ToolStripMenuItem, toolStripSeparator1, saveToFileToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            openFileToolStripMenuItem.Size = new Size(256, 22);
+            openFileToolStripMenuItem.Text = "Load from file...";
+            // 
+            // loadFibonacciNumbersfirst50ToolStripMenuItem
+            // 
+            loadFibonacciNumbersfirst50ToolStripMenuItem.Name = "loadFibonacciNumbersfirst50ToolStripMenuItem";
+            loadFibonacciNumbersfirst50ToolStripMenuItem.Size = new Size(256, 22);
+            loadFibonacciNumbersfirst50ToolStripMenuItem.Text = "Load Fibonacci numbers (first 50)";
+            // 
+            // loadFibonacciNumbersfirst100ToolStripMenuItem
+            // 
+            loadFibonacciNumbersfirst100ToolStripMenuItem.Name = "loadFibonacciNumbersfirst100ToolStripMenuItem";
+            loadFibonacciNumbersfirst100ToolStripMenuItem.Size = new Size(256, 22);
+            loadFibonacciNumbersfirst100ToolStripMenuItem.Text = "Load Fibonacci numbers (first 100)";
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            saveToFileToolStripMenuItem.Size = new Size(256, 22);
+            saveToFileToolStripMenuItem.Text = "Save to File...";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(253, 6);
             // 
             // Form1
             // 
@@ -47,8 +105,12 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(textBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Luke Flock 11759312";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +118,13 @@
         #endregion
 
         private TextBox textBox1;
+        private OpenFileDialog openFileDialog1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openFileToolStripMenuItem;
+        private ToolStripMenuItem loadFibonacciNumbersfirst50ToolStripMenuItem;
+        private ToolStripMenuItem loadFibonacciNumbersfirst100ToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem saveToFileToolStripMenuItem;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Linq;
 using System.Text;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
@@ -9,45 +10,18 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-            RunDistinctIntegers();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void RunDistinctIntegers()
         {
-            // Initialization
-            FindUniques findDuplicates = new FindUniques();
-            StringBuilder results = new StringBuilder();
 
-            // Generate random number list
-            List<int> randomNums = findDuplicates.RandomNumberList(10000, 0, 20000);
-
-            // HashSet Method Output
-            int hashMethodCount = findDuplicates.HashMethod(randomNums);
-            results.AppendLine($"1. HashSet Method: {hashMethodCount} unique numbers");
-            results.AppendLine("    Time Complexity: O(n) where n is the number of items in the list.");
-            results.AppendLine("    Loops through each random number and adds it to a hashset, then returns the hashset length");
-
-            // O(1) Space Method
-            int listMethodCount = findDuplicates.ListMethod(randomNums);
-            results.AppendLine($"2. O(1) Storage Method: {listMethodCount} unique numbers");
-            results.AppendLine("    Time Complexity: O(n^2) where n is the number of items in the list.");
-            results.AppendLine("    For each number in the list, it loops through the entire list until it finds another duplicate, if no duplicate add to unique number counter");
-
-            // Sorted Method Output
-            int sortMethodCount = findDuplicates.HashMethod(randomNums);
-            results.AppendLine($"3. Sorted Method: {sortMethodCount} unique numbers");
-            results.AppendLine("    Time Complexity: O(nlogn) where n is the number of items in the list.");
-            results.AppendLine("    Takes the longest complexity which is the sort method, which averages around O(nlogn)");
 
             // Update text box to proper output
-            textBox1.Text =  results.ToString();
+            textBox1.Text = "Hello World";
         }
 
+        }
     }
 }
 
